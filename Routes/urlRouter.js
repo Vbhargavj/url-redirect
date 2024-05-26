7641f96cd6363a16b390c54241dcd49e88ec04e5
vbj <bbhaijbhai@mail.com>
2024-05-26 18:08:01 +0530

    m

--- Routes/urlRouter.js
+++ Routes/urlRouter.js
@@ -9,6 +9,11 @@ const router = express.Router();
 
 router.route("/url").post(handlePostRequest);
 router.get("/:shortId", redirectHandler);
+router.get("/", (req,res)=>{
+  res.json({
+    "msg":"ok"
+  })
+});
 router.get("/analytic/:shortId",analyticsHandler);
 
 module.exports = router;
